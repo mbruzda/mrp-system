d0cdb13b45808d6cd9f357a0e65ed53dcb52b4a1
OskarKozaczka <oskaro0@vp.pl>
2022-03-10 21:58:25 +0100

    ghp

---
+++ API-ERP/GHP.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.Json;
+using Newtonsoft.Json;
+
+namespace API_ERP
+{
+    public class GHP : IERP
+    {
+
+        public GHPData Data;
+
+
+        public void SetDataFromJson(string jsonString)
+        {
+            var GHPData = JsonConvert.DeserializeObject<GHPData>(jsonString);
+
+            if (!GHPData.SalesForecast.Any()) throw new Exception("Error parsing the table");
+
+            this.Data = GHPData;
+        }
+
+        public void FillTable()
+        {
+
+            this.Data.Inventory[0] = this.Data.StartingInventory;
+
+            for (int i = 1; i < 10; i++)
+            {
+                this.Data.Inventory[i] = this.Data.Inventory[i - 1] - this.Data.SalesForecast[i] + this.Data.Production[i];
+            }
+        }
+
+        public String DataToJson()
+        {
+            return JsonConvert.SerializeObject(this.Data);
+        }
+    }
+}
