f84751c4892a045f2f6c5b7df17a03cdb1d0a557
OskarKozaczka <oskaro0@vp.pl>
2022-03-11 19:22:37 +0100

    Update Program.cs

--- API-ERP/Program.cs
+++ API-ERP/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddCors(options =>
     options.AddDefaultPolicy(
         builder =>
         {
-            builder.AllowAnyOrigin();
+            builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod().DisallowCredentials();
         });
 });
 
