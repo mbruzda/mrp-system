a1b2e24fa2edf6ae45aff0ef334efe48cb7287ff
OskarKozaczka <oskaro0@vp.pl>
2022-03-14 21:51:06 +0100

    SheduledReceipts

--- API-ERP/src/MRPlvl1.cs
+++ API-ERP/src/MRPlvl1.cs
@@ -24,11 +24,9 @@ namespace API_ERP
         {
             var finish = false;
             while (!finish)
-            { 
+            {
                 //GrossRequirements
                 for (int i = MRPData.RealizationTime; i < 10; i++) MRPData.GrossRequirements[i - MRPData.BOM] = GHPData.SalesForecast[i];
-                //SheduledReceipts
-
                 //ProjectedOnHand
                 MRPData.ProjectedOnHand[0] = MRPData.StartingInventory; for (int i = 1; i < 10; i++) MRPData.ProjectedOnHand[i] = MRPData.ProjectedOnHand[i - 1] - MRPData.GrossRequirements[i];
                 //NetRequirements
@@ -41,10 +39,27 @@ namespace API_ERP
                     }
                     if (i == 9) finish = true;
                 }
-                //PlannedRelease
-                for (int i = 0; i < 10; i++) if (MRPData.NetRequirements[i] > 0) MRPData.PlannedRelease[i] = MRPData.LotSize > MRPData.NetRequirements[i] ? MRPData.LotSize : MRPData.LotSize * 2;
-                //PlannedReceipt
-                for (int i = 0; i < 10; i++) if (MRPData.PlannedRelease[i] > 0) MRPData.PlannedReceipt[i - MRPData.RealizationTime] = MRPData.PlannedRelease[i];
+
+                for (int i = 0; i < 10; i++)
+                {
+                    if (MRPData.NetRequirements[i] > 0)
+                    {
+                        if (i>MRPData.RealizationTime) //if this will be true we can start a production, otherwise we need to place an order
+                        {
+                            //PlannedRelease
+                            for (int x = 0; x < 10; x++) if (MRPData.NetRequirements[i] > 0) MRPData.PlannedRelease[i] = MRPData.LotSize > MRPData.NetRequirements[i] ? MRPData.LotSize : MRPData.LotSize * 2;
+                            //PlannedReceipt
+                            for (int x = 0; x < 10; x++) if (MRPData.PlannedRelease[i] > 0) MRPData.PlannedReceipt[i - MRPData.RealizationTime] = MRPData.PlannedRelease[i];
+                        }
+                        else
+                        {
+                            //SheduledReceipts
+                            MRPData.SheduledReceipts[i] = MRPData.GrossRequirements[i]-MRPData.ProjectedOnHand[i];
+                            MRPData.ProjectedOnHand[i] = 0;
+                        }
+
+                    }
+                }
                 //ProjectedOnHandCorrection
                 for (int i = 1; i < 10; i++) MRPData.ProjectedOnHand[i] = MRPData.ProjectedOnHand[i - 1] - MRPData.GrossRequirements[i] + MRPData.PlannedRelease[i];
             }
