c34cce71491e5bc820e5ac6f3f57f66d8d142362
OskarKozaczka <56255713+OskarKozaczka@users.noreply.github.com>
2022-03-30 14:29:35 +0200

    space--

--- API-ERP/Controllers/ERPController.cs
+++ API-ERP/Controllers/ERPController.cs
@@ -60,7 +60,7 @@ namespace API_ERP.Controllers
             try
             {
                 _mrplvl2.SetDataFromJson(jsonString, RT, LS, BOM, SI, AP);
-              //  _mrplvl2.DataCheck();
+              //_mrplvl2.DataCheck();
                 _mrplvl2.FillTable();
                 return _mrplvl2.DataToJson();
             }
@@ -80,7 +80,7 @@ namespace API_ERP.Controllers
             try
             {
                 _mrplvl2.SetDataFromJson(jsonString);
-                //  _mrplvl2.DataCheck();
+                //_mrplvl2.DataCheck();
                 _mrplvl2.FillTable();
                 return _mrplvl2.DataToJson();
             }
